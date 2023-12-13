df1a6cd0f3354c9ec13e2d8657667f892d0c6ff8
Alex Mereuță <mereutaalexandru20@stud.ase.ro>
2023-12-13 23:21:04 +0200

    Add project files

---
+++ src/data/constants.js
@@ -0,0 +1,239 @@
+export const Bio = {
+    name: "Alexandru Mereuță",
+    roles: [
+      "Rookie",
+      "Full Stack Developer",
+      "Android Developer",
+      "Programmer",
+    ],
+    description:
+      "As a passionate Computer Scientist, I excel in developing innovative techniques and transforming them into robust and scalable solutions. My meticulous attention to detail, combined with a high-energy approach, underscores my strong planning and organizational skills. I am enthusiastic about continuous learning, particularly through hands-on projects under expert guidance.",
+    github: "https://github.com/MereutaAlexandru",
+    resume:
+      "https://drive.google.com/file/d/1AnhCmRO7W0AHYtV3KmoU6Wqlj84-XPzd/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/alexandru-ionut-mereuta-b30429279/",
+    insta: "https://www.instagram.com/alex_mereuta/",
+    facebook: "https://www.facebook.com/alex.mereuta.75",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "C#",
+          image:
+            "https://cdn.worldvectorlogo.com/logos/c--4.svg",
+        },
+        {
+          name: "C++",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/306px-ISO_C%2B%2B_Logo.svg.png",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "XML",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+        },
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/C560BAQG1vEeAsco6UQ/company-logo_200_200/0/1519912354526?e=1705536000&v=beta&t=GuEiYkeJ_Ma1WrKD7uO-WU82VVKnsd5GnDgtwh0hamQ",
+      role: "Software Developer Intern",
+      company: "Kepler Management Systems SRL",
+      date: " 2021 February - May",
+      desc: "Design and implementation of customized solutions using the Dynamics NAV and Dynamics 365 Business Central systems.",
+      skills: [
+        ".NET Framework",
+        "C#",
+        "JavaScript",
+        "SQL",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRtIzJZbJ8orQ3HniiwRc_apGA_gjSNNhNTI1uId83Vf7QVNG7A",
+      school: "Faculty of Economic Informatics, Bucharest",
+      date: "2020 - 2024",
+      grade: "--",
+      desc: "I am currently enrolled at university, I have been fortunate to participate in a diverse range of projects. These opportunities have enabled me to explore various areas of technology and data analysis, ultimately leading to valuable experiences and the acquisition of knowledge.",
+      degree: "Bachelor of Economic and Computer Science",
+    },
+    {
+      id: 1,
+      img: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTYNfsPNPP0HxcOlyoMXAjA3XHLwM7b6wmM8I6Z-0VMQp9S6eNH",
+      school: "Oracle PL/SQL",
+      date: "2021 - 2022",
+      grade: "88.2%",
+      desc: "Oracle’s procedural extension language for SQL and the Oracle relational database where we explored the characteristics of PL/SQL and how it is used to extend and automate SQL to administer the Oracle database.",
+      degree: "Database Programming with PL/SQL",
+    },
+    {
+      id: 2,
+      img: "https://www.isf.ro/sites/default/files/inline-images/Logo%20ECDL.jpg",
+      school: "ECDL Romania",
+      date: "2018",
+      grade: "92.3%",
+      desc: "Accreditation of Office 365 knowledge with European Computer Driving License.",
+    },
+    {
+      id: 3,
+      img: "https://www.isf.ro/sites/default/files/inline-images/Logo%20ECDL.jpg",
+      school: "Alexandru Ioan Cuza Theoretical High School",
+      date: "2016 - 2020",
+      grade: "9.3%",
+      desc: "",
+      degree: "Profile: Mathematical - Informatics",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Snake game",
+      date: "2022",
+      description:
+        "The Classic Snake Game is a timeless and entertaining web-based game that challenges players to control a growing snake as it navigates around the screen, collecting food and avoiding collisions with itself and the screen boundaries. This game is developed using HTML, CSS, and JavaScript.",
+      image:
+        "https://github.com/MereutaAlexandru/Snake_game_var1/blob/main/Captur%C4%83%20de%20ecran%202023-10-17%20205839.png?raw=true",
+      tags: [
+        "C#",
+      ],
+      category: "desktop app",
+      github: "https://github.com/MereutaAlexandru/Snake_game_var1",
+    },
+   
+    {
+      id: 1,
+      title: "Todo Web App",
+      date: "Jun 2021",
+      description:
+        " The HTML and CSS-based To-Do Web Application is a sleek and efficient task manager that ensures you never miss an important task, even if you close the browser tab.",
+      image:
+        "https://github.com/MereutaAlexandru/tic-tac-toe/blob/main/Captur%C4%83%20de%20ecran%202023-10-17%20204924.png?raw=true",
+      tags: ["HTML", "CSS"],
+      category: "web app",
+      github: "https://github.com/MereutaAlexandru/To-Do-List",
+      webapp: "https://mereutaalexandru.github.io/To-Do-List/",
+    },
+
+    {
+      id: 2,
+      title: "Tic-tac-toe",
+      date: "Jun 2022",
+      description:
+        " A HTML, CSS, and JavaScript-based Tic-Tac-Toe game is a digital adaptation of the classic paper-and-pencil game. It offers a clean, user-friendly interface and brings the timeless two-player competition to your web browser.",
+      image:
+        "https://github.com/MereutaAlexandru/tic-tac-toe/blob/main/Captur%C4%83%20de%20ecran%202023-10-17%20212512.png?raw=true",
+      tags: ["HTML", "CSS"],
+      category: "web app",
+      github: "https://github.com/MereutaAlexandru/To-Do-List",
+      webapp: "https://mereutaalexandru.github.io/tic-tac-toe/",
+    },
+    {
+      id: 3,
+      title: "Supermarket billing system",
+      date: "2022",
+      description:
+        " A Supermarket Billing System in C++ is a software application designed to manage and streamline the billing process for a supermarket. It offers an efficient and user-friendly solution for store cashiers and customers to generate and track invoices.",
+      image:
+        "https://github.com/MereutaAlexandru/Supermarket-billing-system/blob/main/supermarket.png?raw=true",
+      tags: ["HTML", "CSS"],
+      category: "desktop app",
+      github: "https://github.com/MereutaAlexandru/Supermarket-billing-system",
+    },
+   
+  ];
+  
+ 
+  

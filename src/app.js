765164a237964904dbc5beb85989916c20d1ac44
MereutaAlexandru <115590722+MereutaAlexandru@users.noreply.github.com>
2024-09-23 18:07:01 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 import './app.css';
 import styled, { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,18 +12,18 @@ import Projects from "./components/Projects";
 import Footer from './components/Footer';
 import ProjectDetails from './components/ProjectDetails';
 
-const Body = styled.div
+const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
   height: 100%;
   overflow-x: hidden;
-;
+`;
 
-const Wrapper = styled.div
+const Wrapper = styled.div`
   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
   clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
-;
+`;
 
 function App() {
   const openModalState = { state: false, project: null };
